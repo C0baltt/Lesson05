abd8adcba44c16bfdd042510d09436d37cae0ede
Александр Жаворонок <C0baltt@live.com>
2021-04-15 20:35:51 +0300

    SavePoint

--- TaskManager/TaskManager/TaskManager.cs
+++ TaskManager/TaskManager/TaskManager.cs
@@ -6,50 +6,87 @@ namespace TaskManager
     {
         static void Main(string[] args)
         {
-            
+            RunInLoop();
+            Console.ReadKey();
         }
-    }
-   
-    public class WeeklyTask
-    {
-        private static int counter = 0;
 
-        string nameOfTask = "New task";
-        DateTime taskDate = DateTime.Now;
-
-        public WeeklyTask()
+        private static void RunInLoop()
         {
-            counter++;
+            string input = null;
+
+            while (input != "exit")
+            {
+                PrintMenu();
+
+                input = Console.ReadLine();
+
+                if (int.TryParse(input, out var parsedInput))
+                {
+                    HandleCommand();
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid command. Remember, you must only enter numbers from 1 to 5! Try again, or type \"exit\" to close app");
+                }
+            }
         }
 
-        WeeklyTask(string nameOfTask, DateTime.TryParse(input, out DateTime numberOfArrayElements) )
+        private static void HandleCommand(int parsedInput)
         {
-            
-             
+            switch (parsedInput)
+            {
+                case 1:
+                    HandleAddNewTask();
+                    break;
+                case 2:
+                    HandleList();
+                    break;
+                case 3:
+                    HandleEdit();
+                    break;
+                case 4:
+                    HandleFilterByDate();
+                    break;
+                case 5:
+                    HandleFilterByPriority();
+                    break;
+            }
         }
 
-        public static bool CreateTask(string inputString)
+        private static void PrintMenu()
         {
-            DateTime date;
-            TimeSpan timeSpan;
-
-            var splitParameters = inputString.Split(" ");
-
-            if (splitParameters.Length == 2)
-            {
-                new WeeklyTask(splitParameters[0], splitParameters[1]);
-            }
-            else if (splitParameters.Length == 2)
-            {
-                new WeeklyTask(splitParameters[0], splitParameters[1], splitParameters[2]);
-            }
-            else if (splitParameters.Length == 3)
-            {
-                new WeeklyTask(splitParameters[0], splitParameters[1], splitParameters[2], splitParameters[3]);
-            }
-
-            return true;
+            Console.WriteLine(@"Choose a command:
+1. Add new task
+2. List all tasks
+3. Edit task
+4. Filter by date
+5. Filter by priority");
         }
-        
+
+        private static void HandleAddNewTask()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void HandleList()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void HandleEdit()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void HandleFilterByDate()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void HandleFilterByPriority()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
