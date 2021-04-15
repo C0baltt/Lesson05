0b350ea240713737d65de053c4400264f19138ef
Александр Жаворонок <C0baltt@live.com>
2021-04-15 19:43:42 +0300

    Started to develop TaskManager

--- TaskManager/Temp/Temp.cs
+++ TaskManager/Temp/Temp.cs
@@ -5,36 +5,47 @@ namespace Temp
 {
     class Temp
     {
+        class Account
+        {
+            public Account(decimal sum, decimal rate)
+            {
+                if (sum < MinSum) throw new Exception("Недопустимая сумма!");
+                Sum = sum; Rate = rate;
+            }
+            private static decimal minSum = 100; // минимальная допустимая сумма для всех счетов
+            public static decimal MinSum
+            {
+                get { return minSum; }
+                set { if (value > 0) minSum = value; }
+            }
+
+            public decimal Sum { get; private set; }    // сумма на счете
+            public decimal Rate { get; private set; }   // процентная ставка
+
+            // подсчет суммы на счете через определенный период по определенной ставке
+            public static decimal GetSum(decimal sum, decimal rate, int period)
+            {
+                decimal result = sum;
+                for (int i = 1; i <= period; i++)
+                    result = result + result * rate / 100;
+                return result;
+            }
+        }
         static void Main(string[] args)
         {
-            ArrayList list = new ArrayList();
-            list.Add(2.3); // заносим в список объект типа double
-            list.Add(55); // заносим в список объект типа int
-            list.AddRange(new string[] { "Hello", "world" }); // заносим в список строковый массив
+            Console.WriteLine($"Enter through a space sum, rate and period");
 
-            // перебор значений
-            foreach (object o in list)
-            {
-                Console.WriteLine(o);
-            }
+            string inputString = Console.ReadLine(); 
+            var splitParameters = inputString.Split(" ");
 
-            Console.WriteLine("\n");
-            // удаляем первый элемент
-            list.RemoveAt(0);
-            // переворачиваем список
-            list.Reverse();
-            // получение элемента по индексу
-            Console.WriteLine(list[1]);
-            // перебор значений
+            decimal s = decimal.TryParse(splitParameters[0], out s);
 
-            Console.WriteLine("\n");
+            splitParameters[1];
+            splitParameters[2];
 
-            for (int i = 0; i < list.Count; i++)
-            {
-                Console.WriteLine(list[i]);
-            }
+            GetSum(s, rate, period);
 
-            Console.ReadLine();
+            Console.ReadKey();
         }
     }
 }
