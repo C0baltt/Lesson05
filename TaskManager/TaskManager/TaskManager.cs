55e0d4ee0001e93935fc9527a13bd7b78a50aea2
Александр Жаворонок <C0baltt@live.com>
2021-04-16 20:07:46 +0300

    SavePoint

--- TaskManager/TaskManager/TaskManager.cs
+++ TaskManager/TaskManager/TaskManager.cs
@@ -1,12 +1,12 @@
 ﻿using System;
+using static System.DateTime;
 
 namespace TaskManager
 {
-    class TaskManager
+    public class TaskManager
     {
+        private static WeeklyTaskService s_service = new WeeklyTaskService();
 
-        private static int s_counter;
-        private static WeeklyTask[] tasks = new WeeklyTask[10];
         static void Main(string[] args)
         {
             RunInLoop();
@@ -29,35 +29,14 @@ namespace TaskManager
                 }
                 else
                 {
-                    Console.WriteLine("Invalid command. Remember, you must only enter numbers from 1 to 5! Try again, or type \"exit\" to close app");
+                    Console.WriteLine("Invalid command. Remember, you must only enter numbers from 1 to 5! Try again, or type \"exit\" to close the app");
                 }
-            }
-        }
-
-        private static void HandleCommand(int parsedInput)
-        {
-            switch (parsedInput)
-            {
-                case 1:
-                    HandleAddNewTask();
-                    break;
-                case 2:
-                    HandleList();
-                    break;
-                case 3:
-                    HandleEdit();
-                    break;
-                case 4:
-                    HandleFilterByDate();
-                    break;
-                case 5:
-                    HandleFilterByPriority();
-                    break;
             }
         }
 
         private static void PrintMenu()
         {
+
             Console.WriteLine(@"Choose a command:
 1. Add new task
 2. List all tasks
@@ -66,102 +45,26 @@ namespace TaskManager
 5. Filter by priority");
         }
 
-        private static void HandleList()
+        private static void HandleCommand(int parsedInput)
         {
-            throw new NotImplementedException();
-        }
-
-        private static void HandleEdit()
-        {
-            throw new NotImplementedException();
-        }
-
-        private static void HandleFilterByDate()
-        {
-            throw new NotImplementedException();
-        }
-
-        private static void HandleFilterByPriority()
-        {
-            throw new NotImplementedException();
-        }
-
-        private static void HandleAddNewTask()
-        {
-            if (s_counter > 10)
-            {
-                Console.WriteLine("Out of memory");
-            }
-            Console.WriteLine("Add new task in format: {},{},{}");
-            string inputData = Console.ReadLine();
-            var parts = inputData?.Split(",");
-
-            if (parts == null || parts.Length < 1 || parts.Length > 4)
-            {
-                Console.WriteLine("Invalid task format, try again");
-                return;
-            }
-
-            switch (parts.Length)
+            switch (parsedInput)
             {
                 case 1:
-                    if (s_counter < 10)
-                    {
-                        AddTaskWithName(parts);
-                    }
+                    s_service.HandleAddNewTask();
                     break;
-
                 case 2:
-                    AddTaskWithDate(parts);
+                    s_service.HandleList();
                     break;
-
                 case 3:
-                    AddTaskWithDateTime(parts);
+                    s_service.HandleEdit();
                     break;
-
                 case 4:
-                    AddTaskWithDateAndPriority(parts);
+                    s_service.HandleFilterByDate();
+                    break;
+                case 5:
+                    s_service.HandleFilterByPriority();
                     break;
             }
-        }
-
-        private static void AddTaskWithName(string[] parts)
-        {
-            var task = new WeeklyTask(parts[0]);
-            AddNewTask(task);
-        }
-
-        private static void AddTaskWithDate(string[] parts)
-        {
-            var date = new DateTime.Parse(parts[1]);
-            var task = new WeeklyTask(parts[0], date);
-            AddNewTask(task);
-        }
-
-        private static void AddTaskWithDateTime(string[] parts)
-        {
-            var date = new DateTime.Parse(parts[1]);
-            var time = new DateTime.Parse(parts[2]);
-            var task = new WeeklyTask(parts[0], date, time);
-            AddNewTask(task);
-        }
-
-        private static void AddTaskWithDateAndPriority(string[] parts)
-        {
-            var date = new DateTime.Parse(parts[1]);
-            var time = new DateTime.Parse(parts[2]);
-
-            if (Enum.TryParse<Priority>(parts[3], out var priority))
-            {
-                var task = new WeeklyTask(parts[0], date, time, priority);
-                AddNewTask(task);
-            }
-        }
-
-        private static void AddNewTask(WeeklyTask task)
-        {
-            tasks[s_counter] = task;
-            s_counter++;
         }
     }
 }
---
+++ TaskManager/TaskManager/WeeklyTaskService.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager
+{
+    internal class WeeklyTaskService
+    {
+        private int _counter;
+        private readonly WeeklyTask[] _tasks;
+
+        public WeeklyTaskService()
+        {
+            _tasks = new WeeklyTask[10];
+        }
+
+        public void HandleAddNewTask()
+        {
+            if (_counter >= 10)
+            {
+                Console.WriteLine("Out of memory. Delete unnecessary tasks, and try again");
+                return;
+            }
+            Console.WriteLine("Add new task in format: {},{},{}");
+            string inputData = Console.ReadLine();
+            var parts = inputData?.Split(",");
+
+            if (parts == null || parts.Length < 1 || parts.Length > 4)
+            {
+                Console.WriteLine("Invalid task format, try again");
+                return;
+            }
+
+            switch (parts.Length)
+            {
+                case 1:
+                    if (_counter < 10)
+                    {
+                        AddTaskWithName(parts);
+                    }
+                    break;
+
+                case 2:
+                    AddTaskWithDate(parts);
+                    break;
+
+                case 3:
+                    AddTaskWithDateTime(parts);
+                    break;
+
+                case 4:
+                    AddTaskWithDateAndPriority(parts);
+                    break;
+            }
+        }
+
+        public void HandleList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HandleEdit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HandleFilterByDate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HandleFilterByPriority()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        private void AddTaskWithName(string[] parts)
+        {
+            var task = new WeeklyTask(parts[0]);
+            AddNewTask(task);
+        }
+
+        private void AddTaskWithDate(string[] parts)
+        {
+            var date = new DateTime.Parse(parts[1]);
+            var task = new WeeklyTask(parts[0], date);
+            AddNewTask(task);
+        }
+
+        private void AddTaskWithDateTime(string[] parts)
+        {
+            var date = new DateTime.Parse(parts[1]);
+            var time = new DateTime.Parse(parts[2]);
+            var task = new WeeklyTask(parts[0], date, time);
+            AddNewTask(task);
+        }
+
+        private void AddTaskWithDateAndPriority(string[] parts)
+        {
+            var date = new DateTime.Parse(parts[1]);
+            var time = new DateTime.Parse(parts[2]);
+
+            if (Enum.TryParse<Priority>(parts[3], out var priority))
+            {
+                var task = new WeeklyTask(parts[0], date, time, priority);
+                AddNewTask(task);
+            }
+        }
+
+        private void AddNewTask(WeeklyTask task)
+        {
+            _tasks[_counter] = task;
+            _counter++;
+        }
+    }
+}
