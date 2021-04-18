b63225284b2d378ec8007cb0173b6486994fea38
Александр Жаворонок <C0baltt@live.com>
2021-04-18 20:33:22 +0300

    Save point

--- TaskManager/TaskManager/WeeklyTask.cs
+++ TaskManager/TaskManager/WeeklyTask.cs
@@ -6,13 +6,67 @@ using System.Threading.Tasks;
 
 namespace TaskManagerNamespace
 {
-    internal class PriorityTask : WeeklyTask
+    internal abstract class WeeklyTask
+    {
+        internal string _name;
+        
+        public WeeklyTask()
+        {
+        }
+
+        public WeeklyTask(string name)
+        {
+            _name = name;
+        }
+        
+        public virtual string ConvertToString(int index)
+        {
+            var output = $"Task №{index + 1}: {_name} ";
+            
+            return output;
+        }
+    }
+
+    internal class RegularTask : WeeklyTask
+    {
+        internal DateTime _date;
+        internal DateTime _time;
+
+        public RegularTask()
+        {
+        }
+
+        public RegularTask(string name, DateTime date)
+        {
+            _name = name;
+            _date = date;
+        }
+
+        public RegularTask(string name, DateTime date, DateTime time)
+        {
+            _name = name;
+            _date = date;
+            _time = time;
+        }
+
+        public DateTime GetDate() => _date;
+
+        public override string ConvertToString(int index)
+        {
+            var output = base.ConvertToString(index);
+
+            return output;
+        }
+    }
+
+    internal class PriorityTask : RegularTask
     {
         private readonly Priority _priority;
 
         public Priority GetPriority() => _priority;
+        public DateTime GetDate() => _date;
 
-        public PriorityTask(string name, DateTime date, DateTime time, Priority priority)
+        public PriorityTask(string name, DateTime date, DateTime time, Priority priority) 
         {
             _name = name;
             _date = date;
@@ -27,53 +81,6 @@ namespace TaskManagerNamespace
             if (_priority != Priority.Empty)
             {
                 output += _priority.ToString();
-            }
-
-            return output;
-        }
-    }
-
-    internal class WeeklyTask
-    {
-        internal string _name;
-        internal DateTime _date;
-        internal DateTime _time;
-
-        public WeeklyTask()
-        {
-        }
-
-        public WeeklyTask(string name)
-        {
-            _name = name;
-        }
-
-        public WeeklyTask(string name, DateTime date)
-        {
-            _name = name;
-            _date = date;
-        }
-
-        public WeeklyTask(string name, DateTime date, DateTime time)
-        {
-            _name = name;
-            _date = date;
-            _time = time;
-        }
-
-        public DateTime GetDate() => _date;
-        
-        public virtual string ConvertToString(int index)
-        {
-            var output = $"Task №{index + 1}: {_name} ";
-            if (_date != default)
-            {
-                output += $"{_date.ToShortDateString()} ";
-            }
-
-            if (_time != default)
-            {
-                output += $"{_time.ToShortTimeString()} ";
             }
 
             return output;
